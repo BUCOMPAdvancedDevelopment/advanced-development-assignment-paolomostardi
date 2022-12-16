451a2c6b70b5c515ae1c46c02db209a57424b5d0
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 <paolomostardi@gmail.com>
2022-12-16 11:28:43 +0000

    updating home

--- helper.py
+++ helper.py
@@ -1,13 +1,12 @@
-
 import os
 import google.oauth2.id_token
-import pymysql
+
 import requests
 from datetime import datetime
 
-from google.cloud import datastore
 from google.auth.transport import requests as googleRequests
 from flask import json
+
 firebase_request_adapter = googleRequests.Request()
 db_user = os.environ.get('CLOUD_SQL_USERNAME')
 db_password = os.environ.get('CLOUD_SQL_PASSWORD')
@@ -15,10 +14,13 @@ db_name = os.environ.get('CLOUD_SQL_DATABASE_NAME')
 db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
 
 
+# file used to call help main.py
+# primarily used to call sql functions in the cloud and to authenticate the user
+
+# authenticate the user with fire base
 def authenticateUser(id_token):
     error_message = None
     claims = None
-    times = None
     valid_token = False
     if id_token:
         try:
@@ -33,7 +35,7 @@ def authenticateUser(id_token):
             error_message = str(exc)
     return claims, error_message, valid_token
 
-
+# calls a cloud function to insert an sql query
 def cloud_sql_insert(query):
     url = "https://us-central1-adassigment.cloudfunctions.net/sql_cloud_insert"
     req = requests.post(url, json={
@@ -47,22 +49,25 @@ def cloud_sql_insert(query):
         return None
 
 
+# calls a cloud function to select an sql query
 def cloud_sql_query(query):
     url = "https://europe-west2-adassigment.cloudfunctions.net/helloworld"
     req = requests.post(url, json={
         "query": query,
     }, headers={"Content-type": "application/json", "Accept": "text/plain"})
     result = req.content
-    print("running sql query: ",result)
+    print("running sql query: ", result)
     if result != b'Error: could not handle the request\n':
         return json.loads(result)
     else:
         return None
 
 
+# returns games from an email associated with a user
+# returns none if the query fails
 def get_sql_games_from_email(email, name):
     user_id = get_sql_user_id_from_email(email)
-    print("user id :",user_id)
+    print("user id :", user_id)
     if user_id:
         query = 'SELECT VIDEOGAME_ID from USER_VIDEOGAME WHERE user_id = \'' + str(user_id[0][0]) + '\''
         return cloud_sql_query(query)
@@ -70,6 +75,7 @@ def get_sql_games_from_email(email, name):
     return None
 
 
+# returns the complete list of games from an id
 def find_sql_game_from_id_list(list_id):
     list_game = []
     for game_id in list_id:
@@ -78,21 +84,23 @@ def find_sql_game_from_id_list(list_id):
     return list_game
 
 
-def buy_game(user_id,game_id):
+# make the user buy a game, the user credit is detracted the price of the game
+def buy_game(user_id, game_id):
     user_credit = get_user_credit(user_id)
-    print('user credit: ',user_credit)
+    print('user credit: ', user_credit)
     game_price = get_game_price(game_id)
     insert_success = insert_game_user_table(user_id, game_id)
     if user_credit >= game_price and insert_success:
-        credit_update = update_user_credit(user_credit - game_price,user_id)
-        print('stats after buying: ',user_credit,game_price,user_id,game_id,credit_update,insert_success)
+        credit_update = update_user_credit(user_credit - game_price, user_id)
+        print('stats after buying: ', user_credit, game_price, user_id, game_id, credit_update, insert_success)
         return True
     return False
 
 
-def insert_game_user_table(user_id,game_id):
+def insert_game_user_table(user_id, game_id):
     date = datetime.today().strftime('%Y-%m-%d')
-    query = 'INSERT INTO USER_VIDEOGAME  ( user_id, videogame_id,date_purchased ) VALUES ( \''+str(user_id)+'\', \''+str(game_id)+'\',\''+date+'\');'
+    query = 'INSERT INTO USER_VIDEOGAME  ( user_id, videogame_id,date_purchased ) VALUES ( \'' + str(
+        user_id) + '\', \'' + str(game_id) + '\',\'' + date + '\');'
     return cloud_sql_insert(query)
 
 
@@ -101,8 +109,8 @@ def game_already_bought(user_id, game_id):
     return cloud_sql_query(query)
 
 
-def update_user_credit(user_credit,user_id):
-    query = 'UPDATE USER_TABLE SET credit = '+str(user_credit)+' WHERE user_id = '+str(user_id)+';'
+def update_user_credit(user_credit, user_id):
+    query = 'UPDATE USER_TABLE SET credit = ' + str(user_credit) + ' WHERE user_id = ' + str(user_id) + ';'
     return cloud_sql_insert(query)
 
 
@@ -122,16 +130,15 @@ def get_sql_user_id_from_email(email):
 
 
 def create_sql_user(email, name):
-    query = 'INSERT INTO USER_TABLE  ( email, name ) VALUES ( \''+email+'\', \''+name+'\');'
+    query = 'INSERT INTO USER_TABLE  ( email, name ) VALUES ( \'' + email + '\', \'' + name + '\');'
     return cloud_sql_insert(query)
 
 
 def find_sql_game_from_name(name):
-    query = 'select * from VIDEOGAME WHERE VIDEOGAME_NAME =     \''+name+'\''
+    query = 'select * from VIDEOGAME WHERE VIDEOGAME_NAME =     \'' + name + '\''
     return cloud_sql_query(query)
 
 
 def find_sql_game_from_id(id):
-
-    query = 'select * from VIDEOGAME WHERE VIDEOGAME_ID = '+str(id)
+    query = 'select * from VIDEOGAME WHERE VIDEOGAME_ID = ' + str(id)
     return cloud_sql_query(query)
--- main.py
+++ main.py
@@ -29,21 +29,26 @@ def index():
     return redirect('/home')
 
 
+# the main page of the shop
 @app.route('/home')
 def home():
     user_data, error, valid_token = helper.authenticateUser(request.cookies.get("token"))
     return render_template('home.html', user_data=user_data)
 
 
+# where the user can buy different games
 @app.route('/store')
 def store():
     user_data, error, valid_token = helper.authenticateUser(request.cookies.get("token"))
     print(user_data)
     if valid_token:
         user_id = helper.get_sql_user_id_from_email(user_data['email'])
-        return render_template('store.html', list_game_info=get_list_of_all_games(),user_data=user_data,user_id=user_id)
+        return render_template('store.html', list_game_info=get_list_of_all_games(), user_data=user_data,
+                               user_id=user_id)
     return render_template('store.html', list_game_info=get_list_of_all_games())
 
+
+# here the user can see the they already have
 @app.route('/my_games')
 def my_games():
     user_data, error, valid_token = helper.authenticateUser(request.cookies.get("token"))
@@ -52,12 +57,12 @@ def my_games():
         id_game_list = helper.get_sql_games_from_email(user_data['email'], user_data['name'])
         if id_game_list:
             list_game_info = helper.find_sql_game_from_id_list(id_game_list)
-            print("list of game_info:",list_game_info)
+            print("list of game_info:", list_game_info)
         return render_template('my_games.html', user_data=user_data, list_game_info=list_game_info)
     return redirect('/home')
 
 
-
+# used to make the user login
 @app.route('/login')
 def login():
     user_data, error, valid_token = helper.authenticateUser(request.cookies.get("token"))
@@ -66,7 +71,7 @@ def login():
     return render_template('login.html')
 
 
-
+# used to make the user logout
 @app.route('/logout')
 def logout():
     user_data, error, valid_token = helper.authenticateUser(request.cookies.get("token"))
@@ -81,6 +86,8 @@ def get_list_of_all_games():
     return helper.cloud_sql_query('select * from VIDEOGAME')
 
 
+# generates a dynamic page for a game based on the user
+# if the user is not authenticated it is redirected
 @app.route('/viewGame/<game_id>/<user_id>')
 def view_game(game_id, user_id):
     user_data, error, valid_token = helper.authenticateUser(request.cookies.get("token"))
@@ -91,12 +98,14 @@ def view_game(game_id, user_id):
         print(current_user_id)
         if user_id == str(current_user_id[0][0]):
             game_infos = helper.find_sql_game_from_id(game_id)
-            return render_template('game_page.html', user_id=user_id, game_id=game_id, game_infos=game_infos, user_data=user_data)
+            return render_template('game_page.html', user_id=user_id, game_id=game_id, game_infos=game_infos,
+                                   user_data=user_data)
         return render_template('access_denied.html', user_data=user_data)
     return render_template('login.html')
 
 
-
+# generates a  page for a game based on the user
+# if the user is not authenticated it is redirected
 @app.route('/buyGame/<game_id>/<user_id>')
 def buy_game(game_id, user_id):
     user_data, error, valid_token = helper.authenticateUser(request.cookies.get("token"))
@@ -110,14 +119,16 @@ def buy_game(game_id, user_id):
             success = helper.buy_game(user_id, game_id)
             credit = helper.get_user_credit(user_id)
             if success:
-                return render_template('game_bought.html', user_id=user_id, game_id=game_id, game_infos=game_infos, user_data=user_data, credit = credit)
-            return render_template('game_bought_fail.html', user_id=user_id, game_id=game_id, game_infos=game_infos, user_data=user_data)
+                return render_template('game_bought.html', user_id=user_id, game_id=game_id, game_infos=game_infos,
+                                       user_data=user_data, credit=credit)
+            return render_template('game_bought_fail.html', user_id=user_id, game_id=game_id, game_infos=game_infos,
+                                   user_data=user_data)
         return render_template('access_denied.html', user_data=user_data)
     return render_template('login.html')
 
 
 def tester(asd):
-    return buy_game(7, str(18))
+    return home()
 
 
 if __name__ == '__main__':
